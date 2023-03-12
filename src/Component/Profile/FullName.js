2f55ed4b74710e5bee29dc8d7dc43fe15cc0cb20
soumaya110 <soumayajebali@gmail.com>
2023-03-12 18:09:44 +0100

    first commit

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FullName() {
+  return (
+    <div>
+      <h1>Hello ! I am Soumaya Jebali</h1>
+    </div>
+  )
+}
+
+export default FullName
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePhoto() {
+  return (
+    <div>
+       <img  src="../image.jpg" alt="image" style={{width:"200px",height:"300px",margin:"auto"}} />
+    </div>
+  )
+}
+
+export default ProfilePhoto
