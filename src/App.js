30e532b3234424b7e62818fca2d07822ea0fca63
soumaya110 <soumayajebali@gmail.com>
2023-03-14 13:21:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 
 import './App.css';
-import ProfilePhoto from './Component/Profile/ProfilePhoto';
-import FullName from './Component/Profile/FullName';
-import Address from './Component/Profile/Address';
+import FullName from './Profile/FullName';
+import Bio from './Profile/Bio';
+import Profession from './Profile/Profession';
 
 function App() {
   return (
-    <div className="App">
-    <ProfilePhoto />
-    <FullName />
-    <Address />
+
+    <div >
+      <FullName name ="Soumaya" lastName="Jebali" age="32 years old"/>
+      <Profession profession="I am currently a student" 
+      study=" I'am graduated from Iset'Com"/>
+      <Bio interests="My Interests are"/>
     </div>
   );
 }
